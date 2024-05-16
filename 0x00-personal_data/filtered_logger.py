6bd87ae21025e312efe7bdc41b0b0d0cb20fdf4d
mouad mennioui <moad.menniui@gmail.com>
2024-05-16 19:40:41 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,37 +1,92 @@
 #!/usr/bin/env python3
 """
-This module provides a function to obfuscate specified fields in a log message.
-It is designed to be used where log data
-needs redaction of sensitive information.
+Module for handling Personal Data tasks 0 to 4
 """
-
+import logging
+import mysql.connector
+from os import environ
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor Method """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+def main():
     """
-    Obfuscate specific fields in a log message.
-
-    Args:
-    fields (list): A list of strings representing the fields to obfuscate.
-    redaction (str): The string to replace the contents of the fields with.
-    message (str): The log message containing the fields.
-    separator (str): The character that separates fields in the message.
-
-    Returns:
-    str: The message with specified fields obfuscated.
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
     """
-    pattern = separator.join([f'(?<={field}=)[^;]+' for field in fields])
-    return re.sub(pattern, redaction, message)
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
 
 
-if __name__ == "__main__":
-    # Test code that prints obfuscated messages.
-    fields = ["password", "date_of_birth"]
-    messages = [
-        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
-    ]
-
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
+if __name__ == '__main__':
+    main()
