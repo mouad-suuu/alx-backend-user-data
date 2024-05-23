7a19b7b045eeaef70e018200eccdb99ae6d6939f
mouad mennioui <moad.menniui@gmail.com>
2024-05-23 23:19:39 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,20 +30,24 @@ if auth_type == 'session_exp_auth':
 if auth_type == 'session_db_auth':
     auth = SessionDBAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler."""
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def authenticate_user():
@@ -55,18 +59,15 @@ def authenticate_user():
             "/api/v1/forbidden/",
             "/api/v1/auth_session/login/",
         ]
-        # Check if the request path requires authentication
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            # Abort with 401 if neither auth header nor session cookie is present
             if auth.authorization_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
-            # Abort with 403 if user authentication fails
             if user is None:
                 abort(403)
-            # Assign the authenticated user to request.current_user
             request.current_user = user
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
